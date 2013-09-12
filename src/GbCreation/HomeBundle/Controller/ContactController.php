cbec2cc65efbf833a5bbd88ebadf5fb3accda895
guichou21 <guichou21@gmail.com>
2013-09-12 17:40:21 +0200

    formulaire de contact

--- src/GbCreation/HomeBundle/Controller/ContactController.php
+++ src/GbCreation/HomeBundle/Controller/ContactController.php
@@ -7,8 +7,100 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class ContactController extends Controller
 {
-    
      public function contactAction()
+    {
+        $logger = $this->get('logger');
+        $logger->info('[contactAction] Formulaire de contact');
+        $contact = new Contact();
+        $form   = $this->createForm(new ContactType(), $contact);
+
+         /* On récupère les données du formulaire si il a déjà été passé */
+        $request = $this->get('request');
+
+        /* On ne traite que les données passées en méthode POST */
+        if ($request->getMethod() == 'POST') {
+            /* On applique les données récupérées au formulaire */
+            $form->handleRequest($request);
+            //var_dump($form);
+            //die();
+            /* Si le formulaire est valide, on valide et on redirige vers l'accueil' */
+            if ($form->isValid()) {               
+
+                $logger->info('[contactAction] form is valid');
+                // Bind value with form
+                
+                $data = $form->getData();
+                $logger->info('[contactAction] envoi depuis mail {' + $data->getEmail()+'}');
+                
+                $this->sendEmail($data);
+
+                $logger->info('[contactAction] envoie du mail effectue');
+                $this->get('session')->getFlashBag()->add('notice', 'Votre Email a été envoyé. merci de nous avoir contacté'); 
+                $form   = $this->createForm(new ContactType(), new Contact());
+                //$this->get('session')->setFlash('notice', 'Votre Email a été envoyé. merci de nous avoir contacté');
+            }
+        }
+        
+        return $this->render('GbCreationHomeBundle:Contact:contact.html.twig',
+        array(
+            'form' => $form->createView(),
+        ));
+
+    }
+       
+
+        /**
+         * Send mail 
+         *
+         * @param array $data
+         *
+         */
+        protected function sendEmail($data)
+        {
+
+            $logger = $this->get('logger');
+            $logger->info('[contactAction] sendEmail');
+
+            $emailContent = $data->getContent();
+
+            // On récupère le service antispam
+            $antispam = $this->container->get('gb_creation_wall.antispam');
+
+             // On verifie le contenu du text
+            if ($antispam->isSpam($emailContent)) {
+              $logger->info('[contactAction] Contenu detecte en tant que SPAM');
+              throw new \Exception('Votre message a été détecté comme spam !');
+            }
+
+             $message = \Swift_Message::newInstance()
+                    ->setContentType('text/html')
+                    ->setSubject($data->getSubject())
+                    ->setFrom($data->getEmail())
+                    ->setTo('dev.gbe@gmail.com')
+                    ->setBody($emailContent)
+                ;
+               $this->get('mailer')->send($message);
+        }
+
+
+/* si marche pas - regarder avec le CDATA ?
+                    $message = \Swift_Message::newInstance()
+                        ->setContentType('text/html')
+                        ->setSubject($data['subject'])
+                        ->setFrom($data['email'])
+                        ->setTo('xxxxxx@gmail.com<script type="text/javascript">
+                        /* <![CDATA[ */
+/*                        (function(){try{var s,a,i,j,r,c,l,b=document.getElementsByTagName("script");l=b[b.length-1].previousSibling;a=l.getAttribute('data-cfemail');if(a){s='';r=parseInt(a.substr(0,2),16);for(j=2;a.length-j;j+=2){c=parseInt(a.substr(j,2),16)^r;s+=String.fromCharCode(c);}s=document.createTextNode(s);l.parentNode.replaceChild(s,l);}}catch(e){}})();
+                        /* ]]> */
+ /*                       </script>');
+    */
+
+
+
+
+     /*
+ancienne methode + template mail
+     public function oldcontactAction()
     {
 
     	$contact = new Contact();
@@ -21,13 +113,9 @@ class ContactController extends Controller
 
              if ($form->isValid()) {
 
-               /* 
-               //$process = $formHandler->process();
-               $em = $this->getDoctrine()->getManager();
+               $this->get('session')->getFlashBag()->add('notice', 'Votre Email a été envoyé. merci de nous avoir contacté'); 
+            //$this->get('session')->setFlash('notice', 'Votre Email a été envoyé. merci de nous avoir contacté');
 
-                $this->get('session')->setFlash('notice', 'Merci de nous avoir contacté, nous répondrons à vos questions dans les plus brefs délais.');
-
-                $this->get('session')->getFlashBag()->add('notice', 'Ajout a été effectué');    */
                 $message = \Swift_Message::newInstance()
                     ->setSubject('Hello Email')
                     ->setFrom('dev.gbe@company.com')
@@ -46,6 +134,6 @@ class ContactController extends Controller
                 array(
                     'form' => $form->createView(),
                 ));
-    }
+    }*/
 
 }
